752403ea1df3730fb90a45b9b06d28fa91bedf17
Oleg Basalyga <basalygaoleg@yandex.ru>
2015-01-28 21:40:05 +0500

    Update PartyInAStick.php
    don't forget add use for Stick and Item, Level

--- src/jerlegoman/PartyInAStick/PartyInAStick.php
+++ src/jerlegoman/PartyInAStick/PartyInAStick.php
@@ -3,10 +3,23 @@
 namespace jerlegoman\PartyInAStick;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerItemHeldEvent;
 
-class PartyInAStick extends PluginBase{
-public function onEquip(PlayerItemHeldEvent $e){
-    if($e->getItem() instanceof Stick){
-      $e->getLevel()->dropItem($e->getPlayer(), Item::get(264));
+
+class PartyInAStick extends PluginBase implements Listenter{
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
-}
+    
+    public function onDisable(){
+        
+    }
+    
+    public function onEquip(PlayerItemHeldEvent $e){
+        /* IMPORTANT !!!!!!!!!!!!!!!!! VVVVVVVVVVVV */
+        /* don't forget add use for Stick and Item, Level */
+       if($e->getItem() instanceof Stick){
+         $e->getLevel()->dropItem($e->getPlayer(), Item::get(264));
+       }
+    }
